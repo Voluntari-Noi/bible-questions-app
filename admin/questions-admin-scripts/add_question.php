f5c480efe6e31ac5c48b7f9e7f12b24db8c796e5
GhitaB <ghitabizau27@gmail.com>
2021-11-14 20:00:24 +0200

    Get POST json data and add question in the database.

--- admin/questions-admin-scripts/add_question.php
+++ admin/questions-admin-scripts/add_question.php
@@ -5,12 +5,22 @@
 
   if(isset($_GET['secret'])) {
     if($_GET['secret'] == $secret) {
-      // db_connect();
-      //
-      // db_close();
-      $json_data = $_POST['json_data'];
+      $json_data = json_decode($_POST['json_data'], true);
 
-      $response = $json_data;
+      $response = $json_data['source'];
+      $question_text = $json_data['question_text'];
+      $correct_answer = $json_data['correct_answer'];
+      $incorrect_answer_1 = $json_data['incorrect_answer_1'];
+      $incorrect_answer_2 = $json_data['incorrect_answer_2'];
+      $incorrect_answer_3 = $json_data['incorrect_answer_3'];
+      $source = $json_data['source'];
+
+      db_connect();
+      $sql ="INSERT INTO questions (question_text, correct_answer, incorrect_answer_1, incorrect_answer_2, incorrect_answer_3, source) VALUES ('$question_text', '$correct_answer', '$incorrect_answer_1', '$incorrect_answer_2', '$incorrect_answer_3', '$source')";
+      $result = $conn -> query($sql);
+      db_close();
+
+      $response = "SUCCESS";
     } else {
       $response = "ERROR_INVALID";
     }
