c21e41adbb8a3b8ad70b7c4acc693660e0649f5b
GhitaB <ghitabizau27@gmail.com>
2021-11-14 09:46:44 +0200

    Get values on form submit and convert to JSON.

--- admin/script.js
+++ admin/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-  $("div.questions-list").append("<p>Lista de întrebări</p>");
-
-  $("#questions-form" ).submit(function(event) {
+  function get_form_data() {
+    // Get the data from form submission
     var question_text = $("input#input-question").val();
     var correct_answer = $("input#input-correct-answer").val();
     var incorrect_answer_1 = $("input#input-incorrect-answer-1").val();
@@ -9,7 +8,24 @@ $(document).ready(function() {
     var incorrect_answer_3 = $("input#input-incorrect-answer-3").val();
     var password = $("input#input-password").val();
 
-    console.log(question_text, correct_answer, incorrect_answer_1, incorrect_answer_2, incorrect_answer_3, password);
+    return {
+      'password': password,
+      'question_text': question_text,
+      'correct_answer': correct_answer,
+      'incorrect_answer_1': incorrect_answer_1,
+      'incorrect_answer_2': incorrect_answer_2,
+      'incorrect_answer_3': incorrect_answer_3
+    }
+  }
+
+  $("div.questions-list").append("<p>Lista de întrebări</p>");
+
+  $("#questions-form" ).submit(function(event) {
+    // On form submit
+    var form_data = get_form_data(); // Get the data as submitted by user
+    var json_data = JSON.stringify(form_data); // Convert to JSON
+
+    console.log(json_data);
 
     event.preventDefault();
   });
