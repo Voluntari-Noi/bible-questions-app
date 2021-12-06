60c1aded3ae71941cf1901783627acde5b7ad2a2
GhitaB <ghitabizau27@gmail.com>
2021-12-06 20:51:29 +0200

    Get questions from database.

--- admin/questions-admin-scripts/get_questions.php
+++ admin/questions-admin-scripts/get_questions.php
@@ -1,11 +1,12 @@
 <?php
   header("cache-control: no-cache, no-store, must-revalidate");
   header('Access-Control-Allow-Origin: *');
+  ini_set('display_errors', 1); // TODO remove later
   require_once('config.php');
   require_once('functions.php');
 
   if(isset($_GET['secret'])) {
-    if($_GET['secret'] == $secret) {
+    if($_GET['secret'] == $secret or $_GET['secret'] == $db_token) {
       db_connect();
       $sql ="SELECT * from questions ORDER BY id DESC";
       $result = $conn -> query($sql);
